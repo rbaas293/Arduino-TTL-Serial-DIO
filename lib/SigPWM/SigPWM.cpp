d80a8c422b0c543170bc73490e13475b9487a32c
Rbaas293 <Rybaas293@gmail.com>
2018-08-23 01:43:02 -0400

    Worked on PWM Methods.

--- lib/SigPWM/SigPWM.cpp
+++ lib/SigPWM/SigPWM.cpp
@@ -8,7 +8,9 @@ SigPWM(const uint8_t iPin, const uint8_t iFreq, const uint8_t iDuty, const doubl
   mDirecPin = iDirecPin
   mFreq = iFreq;
   mDuty = iDuty;
+  mDutymicros = iDuty*(1/mFeq);
   mStepsPerRev = iStepsPerRev;
+
 }
 
 ~SigPWM(void){
@@ -19,7 +21,23 @@ int Conv2Steps(double iDegrees){
 //Converts input Degrees into Steps.
 //This Depends on the Steps Per Rev of the motor. this example will use a 2000 Steps/Rev.
 // StepsPerRev = {# of Steps to go 360 Degrees} / {Degrees Per Rev (I would hope this is 360)}
+mStepsPerDeg = mStepsPerRev/360;
+return iDegrees*mStepsPerRev;
 
-Return iDegrees*mStepsPerRev;
+}
 
+void SetFreq(uint8_t iFreq, uint8_t iPeriod_ms){
+
+  if (iFreq == NULL){
+    mFreq = 1/iPeriod_ms;
+    #ifdef DEBUG
+      Serial.print("mFreq = %i set from Period.", mFreq)
+    #endif
+  }
+}
+
+void SetDuty(uint8_t iPercent0_100){
+
+  mDuty = iPercent0_100;
+  mDutymicros = mDuty*(1/mFeq);
 }
--- lib/SigPWM/SigPWM.h
+++ lib/SigPWM/SigPWM.h
@@ -3,18 +3,22 @@
 
 #include <Arduino.h>
 
-Class SigPWM
+class SigPWM
 {
-Public:
-SigPWM(const uint8_t iOutPin, const uint8_t iDisablePin, const uint8_t iDirecPin, const uint8_t iFreq, const uint8_t iDuty);
+public:
+SigPWM(const uint8_t iOutPin, const double iStepsPerRev, const uint8_t iDisablePin, const uint8_t iDirecPin, const uint8_t iFreq, const uint8_t iDuty);
 ~SigPWM(void);
 
 int Conv2Steps(double iDegrees);
-void
+void SetFreq(uint8_t iFreq, uint8_t iPeriod_ms);
+void SetDuty(uint8_t iPercent0_100);
+//bool GreaterThan100();
+protected:
+uint8_t mOutPin, mDisablePin, mDirecPin, mFreq, mDuty, mDutymicros;
+double mStepsPerRev; 
+double mStepsPerDeg;
 
-Protected:
-uint8_t mOutPin, mDisablePin, mDirecPin, mFreq, mDuty;
-double mStepsPerRev;
 
+};
 
-}
+#endif
